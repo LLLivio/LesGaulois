3b3d9837011b9a0184110a95b8a188048f640ece
LLLivio <livio.leufroy@univ-tlse3.fr>
2024-01-17 09:49:39 +0100

    page 8 fait

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,10 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	
+	public Gaulois(String nom, int force) {
+		this.nom=nom;
+		this.force=force;
+		}
 	public String getNom() {
 		return nom;
 		
